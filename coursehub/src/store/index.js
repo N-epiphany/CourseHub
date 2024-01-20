fa7eaae2efbfeffdadda79abadd578b7813a6910
Bhavya Srivastava <bs.epiphany14@gmail.com>
2024-01-20 18:13:19 +0530

    start

--- coursehub/src/store/index.js
+++
@@ -1,8 +0,0 @@
-// store/index.js
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "../reducers/courseReducer";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
